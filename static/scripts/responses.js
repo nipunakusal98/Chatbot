a01563af7b8638fcd62352622bd96071a1ccd682
nipunakusal98 <nipuna.suraweera98@gmail.com>
2023-04-20 12:28:50 +0530

    First Commit

---
+++ static/scripts/responses.js
@@ -0,0 +1,9 @@
+function getBotResponse(input){
+if (input=="hello") {
+	return "Hello!";
+}else if(input == "goodbye") {
+	return "goodbye! have a great day.";
+} else {
+	return "try something else."
+}	
+}
